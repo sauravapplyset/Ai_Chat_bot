3dd1e9a63f6a32c962aced5d097099df95355085
sauravapplyset <saurav.applyset@gmail.com>
2026-05-22 10:42:51 +0530

    Update server.js
    New Chnages

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+console.log("NEW BUILD TEST");
+
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
@@ -5,7 +9,14 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("API Running Successfully");
+  res.send("Cloud Run Working Successfully");
+});
+
+app.get("/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "API Running"
+  });
 });
 
 app.listen(port, "0.0.0.0", () => {
