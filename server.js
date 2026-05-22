ce99f05d2c1d0bbbb4f27aa0e826aab3ae488946
sauravapplyset <saurav.applyset@gmail.com>
2026-05-22 10:36:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,13 @@
-require("dotenv").config();
 const express = require("express");
-// require("./db/redis")
-const cors = require("cors")
+
 const app = express();
-const cookiesParser = require("cookie-parser");
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
-app.use(cors({ origin: "*"}));
-app.use(cookiesParser());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({extended: true}));
-
-// const {middAuth} = require("./middleware/auth");
-// app.use(middAuth)
-const {apiLogger} = require("./middleware/apiLogs");
-app.use(apiLogger)
-
-const userRoutes = require("./routes/user.routes")
-const thirdPartyProvidor = require("./routes/thirdParty.routes");
-const subscription = require("./routes/subscription.routes");
-app.use("/api/user", userRoutes);
-app.use("/api/thirdparty", thirdPartyProvidor);
-app.use("/api/plan", subscription);
-app.use("/api/manage", require("./routes/manageToken.routes"));
-app.use("/api/assi", require("./routes/assistant.routes"));
-app.use("/api/aiModel", require("./routes/aimodel.routes"));
-app.use("/api/media", require("./routes/media.routes"));
-app.use("/api/conversation", require("./routes/conversation.routes"));
-app.use("/api/admin", require("./routes/admin.routes"));
+app.get("/", (req, res) => {
+  res.send("API Running Successfully");
+});
 
 app.listen(port, "0.0.0.0", () => {
-    console.log(`Server Running At PORT : ${port}`);
+  console.log(`Server running on ${port}`);
 });
